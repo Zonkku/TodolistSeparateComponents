e9373412fa87143119d99b7d55c4d850e57378b2
Sonja <sonja.sauvola@gmail.com>
2021-03-09 14:56:58 +0200

    Eka ajo tänne

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,9 +1,14 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 
 export default function Todolist(props) {
 
-
+    const [itemIndex, setItemIndex] = useState(0);
+    
+    const deleteItem = (event) => {
+        setItemIndex(event.target.value);
+        props.todos.filter((todo, i) => i !== itemIndex)
+        
+    }
 
     return (
         <div>
@@ -11,11 +16,15 @@ export default function Todolist(props) {
                 <tbody>
                     <tr><th>Date</th><th>Description</th></tr>
                 {
+                    
                     props.todos.map((todo, index) =>
                         <tr key={index}>
                             <td>{todo.date}</td>
                             <td>{todo.desc}</td>
+                            <td><button name="index" value={index} onClick={deleteItem}>Delete</button></td>
                         </tr>
+                    
+                        
                     )
                 }
                 </tbody>
